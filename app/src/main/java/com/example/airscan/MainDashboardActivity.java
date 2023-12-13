df9df13bd452fe8a8dd75a964c4ef6bf72958223
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2023-12-13 20:21:41 +0700

    merge dashboard

---
+++ app/src/main/java/com/example/airscan/MainDashboardActivity.java
@@ -0,0 +1,14 @@
+package com.example.airscan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maindashboard);
+    }
+}
